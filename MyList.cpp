5bf236fa88e08f903c70c030ea22fd0ce595cb89
Леонид Власов <khromtsov03@mail.ru>
2022-10-02 19:23:21 +0300

    Добавлен метод-геттер GetSize(), модифицирован метод push_back(), добавлена перегрузка оператора индексирования.

--- MyList.cpp
+++ MyList.cpp
@@ -10,6 +10,10 @@ public:
 	List();
 
 	void push_back(T Data);
+
+	int GetSize() const;
+
+	T& operator[] (const int kIndex);
 private:
 	template<typename T>
 	class Node // Создаем вложенный класс для узлов односвязного списка.
@@ -34,6 +38,17 @@ int main()
 	List<int> lst;
 	
 	lst.push_back(5);
+
+	lst.push_back(10);
+
+	lst.push_back(20);
+
+	std::cout << lst[2] << "\n";
+
+	lst[2] = 300;
+
+	std::cout << lst[2] << "\n";
+
 }
 
 template<typename T>
@@ -48,5 +63,39 @@ void List<T>::push_back(T Data)
 {
 	if (pHead == nullptr)
 		pHead = new Node<T>(Data);
+	else
+	{
+		Node<T> *pCurrent = this->pHead;
+
+		while (pCurrent->pNext != nullptr)
+			pCurrent = pCurrent->pNext;
+
+		pCurrent->pNext = new Node<T>(Data);
+	}
+	++iSize;
+}
+
+template<typename T>
+int List<T>::GetSize() const
+{
+	return iSize;
+}
+
+template<typename T>
+T& List<T>::operator[](const int kIndex)
+{
+	int iCounter = 0;
+
+	Node<T> *pCurrent = this->pHead;
+
+	while (pCurrent != nullptr)
+	{
+		if (iCounter == kIndex)
+			return pCurrent->Data;
+
+		pCurrent = pCurrent->pNext;
+
+		++iCounter;
+	}
 }
 
