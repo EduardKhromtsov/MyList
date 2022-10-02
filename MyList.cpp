ae21e8d20c2de611c2f2252bd5840ac6effef82f
Леонид Власов <khromtsov03@mail.ru>
2022-10-02 18:24:39 +0300

    Добавлен класс List, а также вложенный класс Node для узлов списка. Добавлены определения конструктора по умолчанию класса List и метода push_back.

--- MyList.cpp
+++ MyList.cpp
@@ -1,9 +1,52 @@
 ﻿
 #include <iostream>
 
+// Реализация односвязного списка.
+
+template<typename T>
+class List
+{
+public:
+	List();
+
+	void push_back(T Data);
+private:
+	template<typename T>
+	class Node // Создаем вложенный класс для узлов односвязного списка.
+	{
+	public:
+		Node *pNext;
+		T Data;
+
+		Node(T Data = T(), Node *pNext = nullptr)
+		{
+			this->Data = Data;
+			this->pNext = pNext;
+		}
+
+	};
+	Node<T> *pHead; // Указатель на самый первый элемент списка.
+	int iSize; // Количество элементов односвязного списка.
+};
+
 int main()
 {
-    
+	List<int> lst;
+	
+	lst.push_back(5);
 }
 
+template<typename T>
+List<T>::List()
+{
+	iSize = 0;
+	pHead = nullptr;
+}
+
+template<typename T>
+void List<T>::push_back(T Data)
+{
+	if (pHead == nullptr)
+		pHead = new Node<T>(Data);
+}
 
