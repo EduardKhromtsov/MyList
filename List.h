a1cc8f416524090bfebe9890a02a5ef061bc2075
Леонид Власов <khromtsov03@mail.ru>
2022-10-03 14:01:23 +0300

    Добавлен заголовочный файл, в который было перенесено объявление класса.

---
+++ List.h
@@ -0,0 +1,37 @@
+#ifndef LIST_H
+#define LIST_H
+
+template<typename T>
+class List
+{
+public:
+	List();
+
+	void push_back(T Data);
+
+	int GetSize() const;
+
+	T& operator[] (const int kIndex);
+private:
+	template<typename T>
+	class Node // Создаем вложенный класс для узлов односвязного списка.
+	{
+	public:
+		Node* pNext;
+		T Data;
+
+		Node(T Data = T(), Node* pNext = nullptr)
+		{
+			this->Data = Data;
+			this->pNext = pNext;
+		}
+
+	};
+	Node<T>* pHead; // Указатель на самый первый элемент списка.
+	int iSize; // Количество элементов односвязного списка.
+};
+
+#endif 
+
+
+
